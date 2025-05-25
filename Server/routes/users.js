be0af7b60cb2b9e54e119281e9d015efca9bf2ec
Nishant Deshmukh <158269449+nishant-deshmukh@users.noreply.github.com>
2025-05-25 23:22:07 +0530

    Delete Server directory

--- Server/routes/users.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getMyPosts } from "../controllers/post.js"; 
-
-const router = express.Router();
-
-router.get("/me/posts", getMyPosts);
-
-export default router;
